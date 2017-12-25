a67a217b0373a32558cdfc42af390ca36a38b6e9
root <1564223062@qq.com>
2017-12-26 00:21:11 +0800

    Initial commit

---
+++ Emp.java
@@ -0,0 +1,18 @@
+package cn.ylcto.vo;
+
+import java.io.Serializable;
+
+/**
+ * Created by 逍遥阁 on 2017/12/26.
+ */
+public class Emp implements Serializable {
+    private String name="hujianhua";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ js.java
@@ -0,0 +1,7 @@
+package cn.ylcto.vo;
+
+/**
+ * Created by 逍遥阁 on 2017/12/26.
+ */
+public class js {
+}
